33ff1297bf329422ba87efd8fcfe6806e4cd8675
JuanDiegoSalinas1708 <134516091+JuanDiegoSalinas1708@users.noreply.github.com>
2025-10-17 08:01:42 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mini-Project1-React/', // 👈 importante: debe tener slash inicial y final
+  base: '/Project-React/', // 👈 importante: debe tener slash inicial y final
 })
